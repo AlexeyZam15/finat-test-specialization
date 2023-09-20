7e52eb335aa252c360fb247ad3e4e0cc61f8e554
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2023-09-20 17:36:10 +1000

    add Donkeys class and donkeys breed check aux module, added check for int for capacity_kg in PackAnimal class

--- man-friends-program/classes/pack_animals.py
+++ man-friends-program/classes/pack_animals.py
@@ -4,6 +4,8 @@ from classes.parentclass import ParentClass
 class PackAnimal(ParentClass):
     def __init__(self, name: str, birthday: str, capacity_kg: int):
         super().__init__(name, birthday)
+        if not isinstance(capacity_kg, int):
+            raise ValueError("Грузоподъёмность не является числом")
         self.__capacity_kg = capacity_kg
 
     @property
@@ -12,4 +14,4 @@ class PackAnimal(ParentClass):
 
     def __str__(self):
         return f'''{super().__str__()}
-Грузоподъёмность: {self.__capacity_kg}'''
+Грузоподъёмность: {self.__capacity_kg} кг'''
--- man-friends-program/main.py
+++ man-friends-program/main.py
@@ -1,6 +1,7 @@
 from classes.camels import Camel
 from classes.cats import Cat
 from classes.dogs import Dog
+from classes.donkeys import Donkey
 from classes.hamsters import Hamster
 
 
@@ -8,8 +9,9 @@ def main():
     dog1 = Dog("Шарик", "15.04.1998", "Голос, фас, апорт", "Азавак")
     cat1 = Cat("Матроскин", "15.04.1998", "Голос, фу", "Абиссинская")
     hamster1 = Hamster("Хоми", "15.04.1998", "Голос", "Джунгарская")
-    camel1 = Camel("Верблюд1", "15.04.1998", "Голос", "одногорбый верблюд")
-    print(camel1)
+    camel1 = Camel("Верблюд1", "15.04.1998", 250, "одногорбый верблюд")
+    donkey1 = Donkey("Осёл1", "15.04.1998", 100, "пуату")
+    print(donkey1)
 
 
 if __name__ == '__main__':
